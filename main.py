56b27ce9eab5780401c465ee27b215838af035fd
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-13 22:00:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -37,10 +37,17 @@ def main(args) :
     model.eval()
 
 
-    print(f'step 2. inference with model')
-
+    print(f'step 2. file')
     file = 'NFBS/A00028185/sub-A00028185_ses-NFB3_T1w.nii.gz'
     image = sf.load_volume(file)
+    conformed = image.conform(voxsize=1.0,
+                              dtype='float32',
+                              shape=(256, 256, 256), # (256,256,196)
+                              method='nearest',
+                              orientation='LIA')
+    # normalize intensities
+    conformed = (conformed / 255).clip(0, 1)
+
 
     """
     # predict the surface distance transform
